b0956258c978ff742a5860ac6f6453c9a4252f6e
ZhenKyle <zhenkyle@gmail.com>
2016-07-26 09:58:05 +0000

    add buttons action

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,33 @@
 $(function(){
-    $("#bluecircle").percircle({
-      text:"20:00",
-      percent: 20
+    $("#mycircle").percircle({
+      text:$("#work").text(),
+      percent: parseInt($("#work").text())
     });
+
+    $("#work-sub").click(function(){
+      var val = $("#work").text();
+      if (val === "1") {
+        return;
+      }
+      $("#work").text(parseInt(val) -1);
+    });
+
+    $("#work-add").click(function(){
+      var val = $("#work").text();
+      $("#work").text(parseInt(val) +1);
+    });
+
+    $("#rest-sub").click(function(){
+      var val = $("#rest").text();
+      if (val === "1") {
+        return;
+      }
+      $("#rest").text(parseInt(val) -1);
+    });
+
+    $("#rest-add").click(function(){
+      var val = $("#rest").text();
+      $("#rest").text(parseInt(val) +1);
+    });
+
 });
